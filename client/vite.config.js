6cb7f2607bde4887855c8c93c3ebdeb9690fc7b9
utkarsh562 <utkarshpathak562@gmail.com>
2024-09-06 16:26:46 +0530

    Added docker file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Bind to all network interfaces, allowing access from outside the container
+    port: 1000,        // Specify the port (make sure it matches your Docker port mapping)
+    strictPort: true,  // Fail if the port is already in use
+    watch: {
+      usePolling: true // Enable polling for file changes inside the Docker container
+    }
+  }
 })
